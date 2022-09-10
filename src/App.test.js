6c3487f2fc5a2769b3d35e856b0d7f551c18ca9d
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-09-10 18:41:43 -0300

    add acceptance criteria

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+describe('acceptance criteria', () => {
+  it.todo(
+    'when in landing page clicking begin action should navigate to first quiz'
+  );
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  it.todo(
+    'when answering the last quiz should navigate to score page and display results'
+  );
+
+  it.todo(
+    'when in score page, clicking play again action should navigate to landing page'
+  );
 });
